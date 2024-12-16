da4e2a7bcb43536af5eff8d72df9e696a5eb815b
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-12-15 19:31:28 -0500

    Update database.ts

--- src/setup/container/database.ts
+++ src/setup/container/database.ts
@@ -4,13 +4,14 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const database = new Pool({
-    host: process.env.POSTGRES_HOST,
-    port: 5432,
     database: process.env.POSTGRES_DATABASE,
     user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: 5432,
+    options: `--timezone=UTC`,
     idleTimeoutMillis: 1000 * 30,
     max: 10,
-    password: process.env.POSTGRES_PASSWORD
 });
 
 database.on('error', (err, client) => {
