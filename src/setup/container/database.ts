047ed1231f4aa6645417b8ee4db56af5480372ab
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-12-15 19:28:17 -0500

    chore: use connection string & timezone option

--- src/setup/container/database.ts
+++ src/setup/container/database.ts
@@ -2,15 +2,14 @@ import container from '@/lib/container';
 import pg from 'pg';
 
 const { Pool } = pg;
+const { POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
 
 const database = new Pool({
-    host: process.env.POSTGRES_HOST,
-    port: 5432,
     database: process.env.POSTGRES_DATABASE,
-    user: process.env.POSTGRES_USER,
+    connectionString: `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}`,
+    options: `--timezone=UTC`,
     idleTimeoutMillis: 1000 * 30,
     max: 10,
-    password: process.env.POSTGRES_PASSWORD
 });
 
 database.on('error', (err, client) => {
