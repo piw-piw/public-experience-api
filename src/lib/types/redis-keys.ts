d40a9db008733481463d6f8ff483739a2c0f9f77
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-12-22 10:48:56 -0500

    chore: remove old redis keys

--- src/lib/types/redis-keys.ts
+++ src/lib/types/redis-keys.ts
@@ -1,2 +1,31 @@
+import type { ChangelogEntry } from "@/lib/types/experience";
+
+
 export interface RedisKeys {
+    [`changelog:versions_list`]: string[];
+    [`changelog:current_version`]: {
+        id: number;
+        version: string;
+    };
+    [key: `changelog:versions:${string}`]: {
+        _id: number;
+        date: Date;
+        added: string[] | never[];
+        changed: string[] | never[];
+        fixed: string[] | never[];
+    };
+
+    //--
+
+    [`newsletter:current_page`]: string;
+    [`newsletter:pages_list`]: string[];
+    [key: `newsletter:pages:${string}`]: {
+        banner_image_id: string;
+        header_text: string;
+        subheader_text: string;
+        sections: {
+            header: string;
+            content: ({ type: "Paragraph"; text: string; } | { type: "Image";image_id: string; } | { type: "ImageCarousel"; image_ids: string[]; } | { type: "Video"; video_id: string; })[];
+        }[];
+    };
 }
