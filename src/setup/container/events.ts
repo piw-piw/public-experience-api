89baac23885267cd0b6b6335d3e60e4c54e9d57c
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-12-22 10:48:37 -0500

    chore: remove old events

--- src/setup/container/events.ts
+++ src/setup/container/events.ts
@@ -1,14 +1,5 @@
 import EventEmitter from 'events';
-import {
-    getCurrentChangelogs,
-    getCurrentItems,
-    getCurrentNewsletters,
-    getCurrentRockRNG,
-    getCurrentStoreItems,
-    getTranslationStrings
-} from '@/lib/util/execute-luau';
 import container from '@/lib/container';
-import type { ChangelogEntry } from '@/lib/types/experience';
 
 const events = new EventEmitter();
 
@@ -17,71 +8,6 @@ events.on('registered_endpoints', async () => {
 });
 
 events.on('oaklands_update', async ({ curr }: { prev: number; curr: number; }) => {
-    container.logger('Fetching latest news letters.');
-    const newsletters = await getCurrentNewsletters();
-    
-    if (newsletters) {
-        await container.redis.set('news_letters', newsletters);
-    }
-
-    container.logger('Fetching the latest changelogs.');
-    const changelog = await getCurrentChangelogs();
-
-    if (changelog) {
-        const versions: Record<string, Omit<ChangelogEntry, '_id'>> = {};
-        const sorted = Object.entries(changelog) /** The hell I go through to make stuff sorted. */
-            .sort((a, b) => b[1]._id - a[1]._id)
-            .reduce((acc, [version, details]) => ({ ...acc, [version]: details }), {} as Record<string, ChangelogEntry>);
-
-        let latestVersionId = 0;
-        let latestVersion = '0.0.0';
-        for (const [version, details] of Object.entries(sorted)) {
-            if (latestVersionId < details._id) {
-                latestVersionId = details._id;
-                latestVersion = version;
-            }
-
-            /**
-             * @todo: Clean the provided date string up to turn it into a Date object.
-             */
-            const { _id, ...changes } = details;
-            versions[version] = changes;
-        }
-
-        await container.redis.set('changelog', [latestVersion, versions]);
-    }
-
-    container.logger('Fetching current item details.');
-    const itemDetails = await getCurrentItems();
-
-    if (itemDetails) {
-        await container.redis.set('item_details', itemDetails);
-    }
-
-    container.logger('Fetching latest translation strings.');
-    const translationStrings = await getTranslationStrings();
-
-    if (translationStrings) {
-        await container.redis.set('translation_strings', translationStrings);
-    }
-
-    container.logger('Fetching latest shop items.');
-    const shopItems = await getCurrentStoreItems();
-
-    if (shopItems) {
-        await container.redis.set('store_items', shopItems);
-    }
-
-    container.logger('Fetching latest rock rng.');
-    const rockRNG = await getCurrentRockRNG();
-
-    if (rockRNG) {
-        await container.redis.set('current_rock_rng', rockRNG);
-    }
-
-    // Only set it after everything runs, in the event something fails.
-    // This forces it to recache after 5 minutes anyway.
-    await container.redis.set('last_update_epoch', Math.floor(curr));
 });
 
 container.events = events;
