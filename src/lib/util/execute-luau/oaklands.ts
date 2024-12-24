6e32d33b2e1339f3083cac9a26a5c54ae74ed07d
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-12-24 11:52:06 -0500

    chore: readd classic shop method
    this is still needed, im a dummy :bleh:

--- src/lib/util/execute-luau/oaklands.ts
+++ src/lib/util/execute-luau/oaklands.ts
@@ -252,6 +252,39 @@ export async function fetchStoreItems(): Promise<StoresItems> {
 }
 
 /**
+ * Fetch ONLY the classic shop items.
+ * @returns 
+ */
+export async function fetchClassicShopItemsOnly(): Promise<StoresItems> {
+    let script = readLuaFile('./oaklands/classic-shop-items-only.luau');
+
+    const result = await executeLuau<string[]>(script, {
+        universeId: UniverseIDs.Oaklands,
+        placeId: OaklandsPlaceIDs.Production
+    });
+
+    if (!result) return await delayRepoll(fetchClassicShopItemsOnly);
+
+    const results = result.results[0];
+
+    await container.redis.setAdd('oaklands:stores:store_list', 'classic-shop');
+    await container.redis.setAdd(`oaklands:stores:item_list:classic-shop`, results);
+    await container.redis.jsonSet('oaklands:stores:classic_shop_reset', (() => {
+        const reset = new Date();
+
+        if (reset.getUTCHours() >= 16) {
+            reset.setUTCDate(reset.getUTCDate() + 1);
+        }
+
+        reset.setUTCHours(reset.getUTCHours() >= 16 ? 4 : 16, 0, 0, 0);
+
+        return reset;
+    })());
+
+    return { 'classic-shop': results };
+}
+
+/**
  * Fetch the current pirate ship location data.
  * @returns {Promise<ShipLocation>}
  */
