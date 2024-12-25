1988858534907a77d501db623393dc9571aa1f09
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-12-25 11:27:40 -0500

    fix: import database

--- src/setup/initalize.ts
+++ src/setup/initalize.ts
@@ -2,4 +2,5 @@ import "@/setup/env";
 import "@/setup/container/logger";
 import "@/setup/container/redis";
 import "@/setup/container/events";
+import "@/setup/container/database";
 import "@/setup/endpoints";
